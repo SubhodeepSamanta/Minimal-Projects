f3e436525dec102e05c518ccfde81baa6ee2b2c4
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-06-29 14:43:50 +0530

    add beginner

--- TypeScript/app.js
+++ TypeScript/app.js
@@ -1,13 +1,19 @@
 "use strict";
-var StatusCode;
-(function (StatusCode) {
-    StatusCode["statusCode1"] = "hello";
-    StatusCode["statusCode2"] = "world";
-})(StatusCode || (StatusCode = {}));
-const hello = (a, b) => {
-    console.log(a, b);
-    return a;
+let a = "hello";
+let b = 12;
+let c = true;
+let d = [12, 13, 14, 15];
+let e = [12, "Hello"];
+console.log(a, b, c, d, e);
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "going up";
+    Direction["Down"] = "going down";
+    Direction["Left"] = "going left";
+    Direction["Right"] = "going right";
+})(Direction || (Direction = {}));
+console.log(Direction);
+const greet = (name) => {
+    console.log("Hello" + name);
 };
-hello("Subhodeep", "hi");
-hello("deep", "Hi");
-hello("hey", "12");
+greet("Subhodeep");
--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -1,14 +1,29 @@
-enum StatusCode {
-    statusCode1 = "hello",
-    statusCode2 = "world",
+let a: String = "hello";
+let b: Number = 12;
+let c: Boolean = true;
+let d: Number[] = [12,13,14,15];
+let e: [Number,String] = [12, "Hello"];
+
+console.log(a,b,c,d,e);
+
+enum Direction{
+    Up="going up",
+    Down="going down",
+    Left="going left",
+    Right="going right",
 }
 
+console.log(Direction);
 
-const hello= <T>(a: String, b?: T):String=>{
-    console.log(a,b);
-    return a;
+interface Product{
+    id: String,
+    name: String,
+    price: Number,
+    discount?: Number
 }
 
-hello("Subhodeep", "hi");
-hello("deep","Hi");
-hello<String>("hey","12");
+const greet= (name:String):void=>{
+    console.log("Hello " + name);
+}
+
+greet("Subhodeep");
